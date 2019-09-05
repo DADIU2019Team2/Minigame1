3ab65e303e9367895b98be43bc9325b5458b0304
LasseAafeldt <superlla@hotmail.com>
2019-09-05 14:23:15 +0200

    Made player get sucked trhough window on spawn and then made win screen appear when player goes throug window

---
+++ Assets/Scripts/PlayerHitWin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitWin : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject WinScreenOverlay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (LayerMask.NameToLayer("Player") == other.gameObject.layer)
+        {
+            Debug.Log("Player has won and we should run the Win level function");
+            playerWonLevel();
+        }
+    }
+
+    public void playerWonLevel()
+    {
+        WinScreenOverlay.SetActive(true);
+    }
+}
