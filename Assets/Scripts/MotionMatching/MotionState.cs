5f692453611b1ad9884fdaaa88309494f8c14678
BallerJColt <koltai.b@gmail.com>
2019-09-05 10:56:27 +0200

    Animation works
    Animation works via loading the rotations from the CSV file and applying it directly to the model

---
+++ Assets/Scripts/MotionMatching/MotionState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MotionState
+{
+    public int numberOfJoints;
+    [SerializeField]
+    private Quaternion[] jointRotations;
+    private Quaternion[] jointAngularVelocities;
+
+    public MotionState()
+    {
+        numberOfJoints = 0;
+        jointRotations = new Quaternion[0];
+        jointAngularVelocities = new Quaternion[0];
+    }
+
+    public MotionState(Quaternion[] inputArray)
+    {
+        numberOfJoints = inputArray.Length;
+        jointRotations = new Quaternion[numberOfJoints];
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            jointRotations[i] = new Quaternion(inputArray[i].x, inputArray[i].y, inputArray[i].z, inputArray[i].w);
+        }
+
+    }
+
+    public Quaternion[] GetJointRotations() {
+        return jointRotations;
+    }
+
+}
---
+++ Assets/Scripts/MotionMatching/ReadQuaternionCSV.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ReadQuaternionCSV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Quaternion[] rotations;
+    [SerializeField]
+    MotionState[] mST;
+    [SerializeField]
+    private int iterateThroughJoint;
+    public GameObject go;
+    Animator a;
+
+    [SerializeField]
+    string[] boneHeaders;
+    public Transform[] boneTransforms;
+    private int motionStartFrame, motionEndFrame;
+
+    void Awake()
+    {
+        boneHeaders = new string[24];
+        motionStartFrame = 0;
+        motionEndFrame = 3399;
+    }
+
+    void Start()
+    {
+
+        a = go.GetComponent<Animator>();
+        iterateThroughJoint = motionStartFrame;
+        rotations = new Quaternion[24];
+        string path = "Assets/Resources/ft5.csv";
+        string[] everyLine = File.ReadAllLines(path);
+        mST = new MotionState[everyLine.Length - 1];
+        Debug.Log(everyLine.Length);
+        for (int i = 1; i < everyLine.Length; i++)
+        {
+            string[] splitLine = everyLine[i].Split(',');
+
+            for (int j = 0; j < splitLine.Length; j += 4)
+            {
+                rotations[j / 4] = new Quaternion(float.Parse(splitLine[j]), float.Parse(splitLine[j + 1]), float.Parse(splitLine[j + 2]), float.Parse(splitLine[j + 3]));
+            }
+            mST[i - 1] = new MotionState(rotations);
+        }
+        string[] tempHeader = everyLine[0].Split(',');
+        for (int i = 0; i < boneHeaders.Length; i++)
+        {
+            boneHeaders[i] = tempHeader[i * 4];
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+            SetMotionFrameBoundaries(0, 520);
+        if (Input.GetKeyDown("right"))
+            SetMotionFrameBoundaries(520, 855);
+        if (Input.GetKeyDown("down"))
+            SetMotionFrameBoundaries(0, 3399);
+        if (Input.GetKeyDown("left"))
+            SetMotionFrameBoundaries(0, 3399);
+    }
+
+    void LateUpdate()
+    {
+        if (iterateThroughJoint > motionEndFrame)
+            iterateThroughJoint = motionStartFrame;
+        SetBoneRotations(iterateThroughJoint);
+
+        iterateThroughJoint = (iterateThroughJoint += 1);
+    }
+
+    void SetBoneRotations(int frame)
+    {
+        MotionState currentMotionState = mST[frame];
+
+        for (int i = 0; i < boneTransforms.Length; i++)
+            boneTransforms[i].rotation = currentMotionState.GetJointRotations()[i];
+    }
+
+    void SetMotionFrameBoundaries(int lower, int higher)
+    {
+        motionStartFrame = lower;
+        motionEndFrame = higher;
+        iterateThroughJoint = lower;
+        Debug.Log("Frame boundaries:" + lower + " to " + higher);
+    }
+}
