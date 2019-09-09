5e76eb067ef3bdf2cf8c51ed4e49c121fc9c2ccc
Rutaceae <jamiemoeller@yahoo.dk>
2019-09-09 11:04:48 +0200

    navMeshAgent and walking on floor/ceiling working
    monster navmesh set up and working on prototype level.

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent navAgent;
+    [SerializeField] private Transform playerTransform;
+
+    private float timer = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (navAgent == null) { navAgent = GetComponent<NavMeshAgent>(); }
+        if (playerTransform == null) { playerTransform = GameObject.FindGameObjectWithTag("Player").transform; }
+
+        navAgent.autoRepath = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Is agent on navmesh? " + navAgent.isOnNavMesh);
+        navAgent.destination = playerTransform.position;
+    }
+
+}
