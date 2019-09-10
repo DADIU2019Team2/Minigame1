14edecde88f50cdc8e9aedf29992b412127ad843
LasseAafeldt <superlla@hotmail.com>
2019-09-10 10:46:41 +0200

    level 1 should be plyable, done a bit in levvel 2 to make it playable

---
+++ Assets/Scripts/GlassCabinetBreak.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassCabinetBreak : MonoBehaviour
+{
+    bool enableCollisionBreaking = false;
+
+    [SerializeField]
+    private GameObject actualBall;
+
+    private void Start()
+    {
+        StartCoroutine(enableBreakingCollisions(1f));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (enableCollisionBreaking.Equals(true))
+        {
+            Transform[] children = GetComponentsInChildren<Transform>();
+            children[0] = null; //remove the parent from the array
+
+            //kill glass
+            children[children.Length - 1].gameObject.SetActive(false);
+            //remove ball
+            GameObject fakeBall = GetComponent<SphereCollider>().gameObject;
+            fakeBall.SetActive(false);
+            actualBall.transform.position = fakeBall.transform.position;
+            actualBall.transform.rotation = fakeBall.transform.rotation;
+            actualBall.SetActive(true);
+        }
+    }
+
+    IEnumerator enableBreakingCollisions(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        enableCollisionBreaking = true;
+    }
+}
