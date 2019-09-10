ece405ee97f47e624655d25ffb5caab51d7f72cb
BallerJColt <koltai.b@gmail.com>
2019-09-10 10:37:45 +0200

    AutoTranslator Works
    Autotranslator needs to be attached to UI elements with text and their ID needs to be set in the inspector

---
+++ Assets/Scripts/AutoTranslator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class AutoTranslator : MonoBehaviour
+{
+    public string textKey;
+
+
+    void OnEnable()
+    {
+        OnLanguageChange();
+    }
+    public void OnLanguageChange()
+    {
+        Text textElement = GetComponent<Text>();
+        textElement.text = LocalizationManager.TranslateKey(textKey);
+    }
+}
